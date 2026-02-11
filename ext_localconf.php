fd1bb13e69e4fab536ae4393cef54171d7bddac5
Niklas Naumann <dev@niklasnaumann.de>
2026-02-11 23:51:54 +0100

    Use css

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,3 @@ defined('TYPO3') or die('Access denied.');
 
 // Add default RTE configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sjsh_website'] = 'EXT:sjsh_website/Configuration/RTE/Default.yaml';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-  @import "EXT:sjsh_website/Configuration/TypoScript/setup.typoscript"
-');
